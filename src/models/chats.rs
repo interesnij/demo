fcb09fd014ca58355c0ba92970901b27521a2a8b
interesnij <ochkarik1983@mail.ru>
2022-06-19 15:02:27 +0300

    J

--- src/models/chats.rs
+++ src/models/chats.rs
@@ -215,7 +215,7 @@ impl Chat {
 
         let _connection = establish_connection();
         let new_chat_form = NewChat {
-            name: Some(name[..100].to_string()),
+            name: Some(name.unwrap()[..100].to_string()),
             types: types,
             community_id: community_id,
             user_id: creator.id,
@@ -273,7 +273,7 @@ impl Chat {
         let _connection = establish_connection();
 
         let edit_chat_form = EditChat {
-            name: Some(name[..100].to_string()),
+            name: Some(name.unwrap()[..100].to_string()),
             image: image,
             description: description,
             reactions: reactions,
