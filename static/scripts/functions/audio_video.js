bf856180811248629ffb29a3248cc412ab33cf44
interesnij <ochkarik1983@mail.ru>
2022-06-21 10:26:37 +0300

    H

--- static/scripts/functions/audio_video.js
+++ static/scripts/functions/audio_video.js
@@ -494,6 +494,7 @@ function show_play_items(block, track_id) {
 }
 
 function get_music_player_support(block) {
+  console.log("Time" + music_player.getCurrentTime());
   if (music_player.getCurrentTime() && music_player.getCurrentTime() != "00:00") {
     show_play_items(block, $playlist.getAttribute("data-pk"));
   }
