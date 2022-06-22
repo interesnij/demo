e6b375541f2a5f37cbb77da76e7385da43352fe2
interesnij <ochkarik1983@mail.ru>
2022-06-22 10:27:23 +0300

    J

--- static/scripts/functions/audio_video.js
+++ static/scripts/functions/audio_video.js
@@ -562,7 +562,11 @@ function get_music_player_support(block) {
 function dragElement(elmnt){var pos1=0,pos2=0,pos3=0,pos4=0;document.querySelector("#draggable-header").onmousedown=dragMouseDown;document.querySelector("#draggable-resize").onmousedown=resizeMouseDown;function dragMouseDown(e){e=e||window.event;e.preventDefault();pos3=e.clientX;pos4=e.clientY;document.onmouseup=closeDragElement;document.onmousemove=elementDrag}function resizeMouseDown(e){e=e||window.event;e.preventDefault();pos3=0;pos4=0;document.onmouseup=closeDragElement;document.onmousemove=elementResize}function elementResize(e){e=e||window.event;e.preventDefault();var content=document.querySelector(".draggable");var width=content.offsetWidth;var height=content.offsetHeight;pos1=(e.clientX-width)-content.offsetLeft;pos2=(e.clientY-height)-content.offsetTop;content.style.width=width+pos1+'px';content.style.height=height+pos2+'px'}function elementDrag(e){e=e||window.event;e.preventDefault();pos1=pos3-e.clientX;pos2=pos4-e.clientY;pos3=e.clientX;pos4=e.clientY;elmnt.style.top=(elmnt.offsetTop-pos2)+"px";elmnt.style.left=(elmnt.offsetLeft-pos1)+"px"}function closeDragElement(){document.onmouseup=null;document.onmousemove=null}}
 
 
-on('#ajax', 'click', '.music_list_item', function() {
+on('#ajax', 'click', '.music_list_item', function(e) {
+  if (e.target.classList.contains("play_list_mode")) {
+    console.log("play_list_mode!!");
+    return;
+  }
   track = this.parentElement.parentElement.parentElement;
   if (track.querySelector(".progress2").getAttribute("style")) {
     if (track.classList.contains('pause')) {
