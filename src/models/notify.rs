ec231d8adb261b0b87f11b25e4b6eb44a08e3db3
interesnij <ochkarik1983@mail.ru>
2022-06-26 13:38:33 +0300

    J

--- src/models/notify.rs
+++ src/models/notify.rs
@@ -89,7 +89,7 @@ impl Notification {
 
         let creator_id = creator.id;
         let _connection = establish_connection();
-        let current_verb = creator.get_verb_gender(&verb).clone();
+        let current_verb = &creator.get_verb_gender(&verb);
         let users_ids = creator.get_users_ids_for_main_news();
         let date = chrono::Local::now().naive_utc();
 
@@ -171,7 +171,7 @@ impl Notification {
                         Notification::create_notify (
                             creator_id,
                             *user_id,
-                            current_verb.clone(),
+                            current_verb,
                             types,
                             object_id,
                             None,
