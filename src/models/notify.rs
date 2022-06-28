800c85e9ba08262afa18a3aee895ecfb1566d3a4
interesnij <ochkarik1983@mail.ru>
2022-06-28 15:35:07 +0300

    J

--- src/models/notify.rs
+++ src/models/notify.rs
@@ -161,7 +161,7 @@ impl Notification {
 
     pub fn get_current_notify(&self) -> &Notification {
         if self.is_have_object_set() {
-            return self.get_first_object_set();
+            return &self.get_first_object_set();
         }
         else if self.is_have_user_set() {
             return self.get_first_user_set();
