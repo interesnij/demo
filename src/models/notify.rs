d193088b3776733653d944d3481ab7afeb3a8bec
interesnij <ochkarik1983@mail.ru>
2022-06-29 16:16:25 +0300

    J

--- src/models/notify.rs
+++ src/models/notify.rs
@@ -550,12 +550,12 @@ impl WallObject {
         };
     }
     pub fn get_verb(&self) -> String {
-        let verb: &str;
+        let verb: String;
         if self.types > 80 && self.types < 100 {
-            verb = &(self.verb + &self.get_comment_verb());
+            verb = self.verb + &self.get_comment_verb();
         }
         else {
-            verb = &self.verb;
+            verb = self.verb;
         }
 
         if verb.contains("опуб") {
