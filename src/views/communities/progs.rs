e3d1ecef9886021157b2ef234b49020aad15fce6
interesnij <ochkarik1983@mail.ru>
2022-07-02 13:15:44 +0300

    J

--- src/views/communities/progs.rs
+++ src/views/communities/progs.rs
@@ -80,7 +80,7 @@ pub async fn create_community(session: Session, req: HttpRequest, mut payload: M
             &_request_user,
             form.types,
         );
-        return community_page(session, req, new_community_link.link).await;
+        return community_page(session, req, new_community_link).await;
 
     } else {
         Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(""))
